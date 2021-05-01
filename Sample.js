922d7911e48f408b1c2bf0bf4b6a2af59f0f8439
SAQRAL325 <82032248+SAQRAL325@users.noreply.github.com>
2021-05-01 12:44:18 +0300

    Add files via upload

---
+++ Sample.js
@@ -0,0 +1,47 @@
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('Muhanad.db' , (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the Blog SQlite database');
+});
+
+
+
+//لعرض مواعيد اليوم
+db.all('SELECT  Name,Email,Title,Date,time FROM Contacts JOIN Dates ON Contacts.DatesID = Dates.DatesID WHERE Date="2021-04-29"', function(err, table){
+    if(err){
+        return console.log(err.message);
+    }
+    console.log(table);
+});
+
+
+/*
+//لعرض مواعيد الاسبوع
+db.all('SELECT  Name,Email,Title,Date,time FROM Contacts JOIN Dates ON Contacts.DatesID = Dates.DatesID WHERE Date>="2021-04-29" AND Date<="2021-05-06"', function(err, table){
+    if(err){
+        return console.log(err.message);
+    }
+    console.log(table);
+});
+*/
+
+
+/*
+//لعرض مواعيد الشهر
+db.all('SELECT  Name,Email,Title,Date,time FROM Contacts JOIN Dates ON Contacts.DatesID = Dates.DatesID WHERE Date>="2021-04-29" AND Date<="2021-05-29"', function(err, table){
+    if(err){
+        return console.log(err.message);
+    }
+    console.log(table);
+});
+*/
+
+db.close( (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+});
